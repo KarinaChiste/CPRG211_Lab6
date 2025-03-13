2532ae8b5d99ee835fef3bffb3c827095b67a38b
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-03-13 08:02:48 -0600

    Serialization and Deserialization Complete

---
+++ Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class Event
+    {
+        public int EventNumber {  get; set; }
+        public string Location { get; set; }
+        public string EventName { get; set; }
+
+        public Event(int eventNumber, string location, string eventName)
+        {
+            EventNumber = eventNumber;
+            Location = location;
+            EventName = eventName;
+        }
+        public override string ToString()
+        {
+            return $"{EventNumber}\n{Location}\n{EventName}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Lab6
+﻿using System.Text.Json;
+
+namespace Lab6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Event myEvent = new Event(1, "Calgary", "Tech Competition");
+
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(myEvent, options);
+            File.WriteAllText("event.txt", jsonString);
+
+            var data = File.ReadAllText("event.txt");
+            Event output = JsonSerializer.Deserialize<Event>(data);
+            Console.WriteLine(output);
+
+
+            
         }
     }
 }
